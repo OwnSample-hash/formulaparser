9d4c8783fd62238be3e2060e4b0a34f6b023361c
OwnSample <ownsample@tutanota.com>
2023-10-06 09:44:47 +0200

    Fixed !A!B now should parse to the correctly

--- parser/parser.py
+++ parser/parser.py
@@ -9,24 +9,27 @@ class Parser:
         self.input_str = input
 
     def __iter__(self):
-        self.ogp = self.input_str
-        iter_in = iter(self.ogp.upper())
+        iter_in = iter(self.input_str.upper())
         prev_c = ''
         for char in iter_in:
+            # print("prev_c:",prev_c)
             if char in whitespace:
                 continue
             out = char
             if char in ascii_uppercase and prev_c[-1:] in ascii_uppercase:
                 out = f' and {char}'
             if char == '!':
-                if prev_c == '(':
+                if prev_c == '(' or prev_c[-1:] == ' ':
                     out = f'not {next(iter_in)}'
+                elif prev_c[-1:] in ascii_uppercase and prev_c:
+                    out = f' and not {next(iter_in)}'
                 else:
                     out = f' not {next(iter_in)}'
             if char == '+':
                 out = ' or '
             prev_c = out
             yield out
+        # print("parsed:",self.input_str, "=V",)
         return
 
     def parse(self, input_:str)->str:
@@ -41,7 +44,12 @@ class Parser:
 
 
 if __name__ == '__main__':
-    parsed = ""
-    for pc in Parser('!a!B'):
+    p = Parser('!a')
+    parsed = ''
+    for pc in p.parse('!a!b'):
+        parsed += pc
+    print(parsed)
+    parsed = ''
+    for pc in p.parse('(!a!B)+(!A+!BC)'):
         parsed += pc
     print(parsed)
