f4c06d3102e9fe852a8f50d8a48e29edb3d63adc
OwnSample <ownsample@tutanota.com>
2023-10-07 12:47:07 +0200

    Refactord main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/bin/env python3
+from string import ascii_uppercase
 from parser.evaler import Evaler
 from parser.parser import Parser
 from utils import *
@@ -17,11 +18,21 @@ q or eof to quit
     )
 
 
+def do(prompt: str):
+    prompt = parser.parse(prompt)
+    for i in range(size):
+        print(ascii_uppercase[i], end=' ')
+    prompt = dedupwc(prompt)
+    print(f'{prompt}')
+    for evc in ev(prompt):
+        print(evc)
+
+
 if __name__ == '__main__':
     help()
     size = get_size()
     ev = Evaler(size)
-    parser = Parser("A")
+    parser = Parser('')
     last_evaled_prompt = ''
     while 1:
         try:
@@ -43,6 +54,7 @@ if __name__ == '__main__':
         if prompt.startswith('RE'):
             size = int(prompt.split(' ')[1])
             print('Resized table to', size)
+            ev.re_size(size)
             continue
 
         if prompt == '?':
@@ -55,7 +67,7 @@ if __name__ == '__main__':
                 continue
             fn = input('Filename>')
             print(f'Saving to {fn}')
-            with open(fn, "a") as o:
+            with open(fn, 'a') as o:
                 osout = sys.stdout
                 sys.stdout = o
                 do(last_evaled_prompt)
@@ -69,7 +81,7 @@ if __name__ == '__main__':
                 continue
             fn = ogp.split(' ')[1]
             print(f'Saving to {fn}')
-            with open(fn, "a") as o:
+            with open(fn, 'a') as o:
                 osout = sys.stdout
                 sys.stdout = o
                 do(last_evaled_prompt)
@@ -97,11 +109,5 @@ if __name__ == '__main__':
             print('Empty prompt try again!')
             continue
 
-        for i in range(size):
-            print(ascii_uppercase[i], end=' ')
-        prompt = dedupwc(prompt)
-        print(f'{check_dm(prompt)}')
-        parsed = parser.parse(prompt)
-        for evc in ev(parsed):
-                print(evc)
+        do(prompt)
         last_evaled_prompt = prompt
