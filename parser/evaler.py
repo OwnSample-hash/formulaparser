a8ee62a25a827fb6f62fc83d41316d79567e31bb
OwnSample <ownsample@tutanota.com>
2023-10-07 16:45:51 +0200

    Addede typing hinting, raw mode where it only returns the int and nothing else

--- parser/evaler.py
+++ parser/evaler.py
@@ -1,20 +1,20 @@
 from string import ascii_uppercase   # pyright: ignore
-from typing import List
+from typing import Iterator, List
 from utils.mktbl import make_table
 
 
 class Evaler:
     def __init__(self, size: int) -> None:
-        self.size = size
+        self.size: int = size
         self.table: List[List[str]] = make_table(self.size)
         self.more_info: bool = False
 
-    def re_size(self, new_size):
-        self.size = new_size
+    def re_size(self, new_size: int) -> None:
+        self.size: int = new_size
         self.table: List[List[str]] = make_table(self.size)
         self.prompt: str = ''
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[str | int]:
         abort_ = False
         for it in self.table:   # pyright: ignore
             for i in range(self.size):
@@ -22,9 +22,17 @@ class Evaler:
                 exec(f'print({ascii_uppercase[i]}, end=" ")')
             try:
                 # print(self.prompt)
-                yield '| {0}{1}'.format(int(eval(self.prompt)) ,(
-                    '\t' + self.prompt
-                ) if self.more_info else '')
+                if not self.raw:
+                    yield '| {0}{1}'.format(
+                        int(eval(self.prompt)),
+                        ('\t' + self.prompt) if self.more_info else '',
+                    )
+                else:
+                    yield int(eval(self.prompt))
+            except NameError:
+                print('\n\nUse re "<num>" to set variable size')
+                abort_ = True
+                break
             except:
                 print(
                     '\nErr happend in evaluation of the expression\nAborting...'
@@ -35,6 +43,7 @@ class Evaler:
                 break
         return
 
-    def __call__(self, prompt: str):
+    def __call__(self, prompt: str, raw: bool = False):
         self.prompt = prompt
+        self.raw = raw
         return self
