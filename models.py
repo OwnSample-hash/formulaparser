752d08955da41b17a9c506c5753c8f1b952fc9ec
OwnSample <ownsample@tutanota.com>
2023-10-07 16:33:56 +0200

    Removed EXPR_RES and merged it to it's referd palce

--- models.py
+++ models.py
@@ -2,14 +2,9 @@ from pydantic import BaseModel
 from typing import List
 
 
-class EXPR_RES(BaseModel):
-    name: str
-    values: List[int]
-
-
 class EXPR(BaseModel):
     # id: int
     name: str
     expr_raw: str
     expr_par: str
-    res: EXPR_RES | None = None
+    res: List[int] | None = None
