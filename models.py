5dc0d20afce7f61779d1486d44cfb5c8ca370456
OwnSample <ownsample@tutanota.com>
2023-10-09 19:11:15 +0200

    Removed dependecy for pydantic

--- models.py
+++ models.py
@@ -1,10 +1,18 @@
-from pydantic import BaseModel
 from typing import List
 
 
-class EXPR(BaseModel):
+class EXPR:
     id: str
     name: str
     expr_raw: str
     expr_par: str
     res: List[int]
+
+    def __init__(
+        self, id: str, name: str, expr_raw: str, expr_par: str, res: List[int]
+    ):
+        self.id = id
+        self.name = name
+        self.expr_raw = expr_raw
+        self.expr_par = expr_par
+        self.res = res
